b1c82e894a1772598484927fab29887a8060ac7f
dmdsouza <dnldsz9@gmail.com>
2023-11-27 13:38:30 -0800

    fix #8

--- mtr/datasets/waymo/data_preprocess.py
+++ mtr/datasets/waymo/data_preprocess.py
@@ -17,6 +17,8 @@ from waymo_types import object_type, lane_type, road_line_type, road_edge_type, 
 
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(physical_devices[0], True)
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(physical_devices[1], True)
 gpus = tf.config.list_physical_devices('GPU')
 # if gpus:
 #   # Restrict TensorFlow to only use the first GPU
