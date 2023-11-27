87f27a091830ad51993a135eb8d29747a9c0e333
dmdsouza <dnldsz9@gmail.com>
2023-11-27 14:20:46 -0800

    fix #14

--- mtr/datasets/waymo/data_preprocess.py
+++ mtr/datasets/waymo/data_preprocess.py
@@ -15,8 +15,8 @@ from tqdm import tqdm
 from waymo_open_dataset.protos import scenario_pb2
 from waymo_types import object_type, lane_type, road_line_type, road_edge_type, signal_state, polyline_type
 
-physical_devices = tf.config.experimental.list_physical_devices('GPU')
-tf.config.experimental.set_memory_growth(physical_devices[0], True)
+# physical_devices = tf.config.experimental.list_physical_devices('GPU')
+# tf.config.experimental.set_memory_growth(physical_devices[0], True)
 # physical_devices = tf.config.experimental.list_physical_devices('GPU')
 # tf.config.experimental.set_memory_growth(physical_devices[1], True)
 # gpus = tf.config.list_physical_devices('GPU')
@@ -29,6 +29,18 @@ tf.config.experimental.set_memory_growth(physical_devices[0], True)
 #   except RuntimeError as e:
 #     # Visible devices must be set before GPUs have been initialized
 #     print(e)
+
+gpus = tf.config.list_physical_devices('GPU')
+if gpus:
+  try:
+    # Currently, memory growth needs to be the same across GPUs
+    for gpu in gpus:
+      tf.config.experimental.set_memory_growth(gpu, True)
+    logical_gpus = tf.config.list_logical_devices('GPU')
+    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+  except RuntimeError as e:
+    # Memory growth must be set before GPUs have been initialized
+    print(e)
 
 
 from waymo_open_dataset import dataset_pb2
