d72cd68fc504baad31ecf13645e257ca7ed16ef0
dmdsouza <dnldsz9@gmail.com>
2023-11-27 15:59:44 -0800

    added extracted point clouds

--- mtr/datasets/waymo/data_preprocess.py
+++ mtr/datasets/waymo/data_preprocess.py
@@ -273,11 +273,11 @@ def process_waymo_data_with_scenario_proto(data_file, output_path=None):
         scenario_augmented = womd_lidar_utils.augment_womd_scenario_with_lidar_points(
             scenario, womd_lidar_scenario)
         # print("here")
-        # frame_points_xyz, frame_points_feature, frame_i = _extract_point_clouds(scenario_augmented)
-        # print("finished")
-        # info['frame_points_xyz'] = frame_points_xyz
-        # info['frame_points_feature'] = frame_points_feature
-        # info['frame_i'] = frame_i
+        frame_points_xyz, frame_points_feature, frame_i = _extract_point_clouds(scenario_augmented)
+        print("finished")
+        info['frame_points_xyz'] = frame_points_xyz
+        info['frame_points_feature'] = frame_points_feature
+        info['frame_i'] = frame_i
 
         info['scenario_id'] = scenario.scenario_id
         info['timestamps_seconds'] = list(scenario.timestamps_seconds)  # list of int of shape (91)
