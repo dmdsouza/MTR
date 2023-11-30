270caced555b1a8bd30d3752f9b53028d0cf7746
dmdsouza <dnldsz9@gmail.com>
2023-11-30 13:56:29 -0800

    reduced number of workers

--- mtr/datasets/waymo/data_preprocess.py
+++ mtr/datasets/waymo/data_preprocess.py
@@ -394,7 +394,7 @@ def get_infos_from_protos(data_path, output_path=None, num_workers=4):
     return all_infos
 
 
-def create_infos_from_protos(raw_data_path, output_path, num_workers=8):
+def create_infos_from_protos(raw_data_path, output_path, num_workers=4):
     train_infos = get_infos_from_protos(
         data_path=os.path.join(raw_data_path, 'training'),
         output_path=os.path.join(output_path, 'processed_scenarios_training'),
